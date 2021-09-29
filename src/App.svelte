b36c6fad9dc78b441b1de0cf4de29d144a92842c
Thibaut <thibaut.musslin@gmail.com>
2021-09-29 11:11:24 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Le site ne devrait pas ressembler a ca très longtemps !</p>
+</main>
+
+<style>
+	main {
+		font-family: "JetBrains Mono",serif;
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #1d89f9;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
