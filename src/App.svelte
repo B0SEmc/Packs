455379e4b35b02a75ac015568018a1bfb093ce1e
Thibaut <thibaut.musslin@gmail.com>
2021-10-03 14:45:29 +0200

    nice changes

--- src/App.svelte
+++ src/App.svelte
@@ -4,31 +4,32 @@
 <!-- svelte-ignore a11y-invalid-attribute -->
 <main>
   <div>
-    <nav class="flex mx-auto bg-black h-auto bg-opacity-20 shadow-xl">
+    <nav id="nav" class="flex bg-black bg-fixed bg-cover h-auto bg-opacity-20 shadow-xl">
       <div>
         <a href="https://packs.netlify.com">
-          <img class="pl-16 py-1.5" src="images/Icon.png" alt="Icon" />
+          <img class="pl-16 py-1.5 transform transition duration-150 ease-in-out hover:scale-110" src="images/Icon.png" alt="Icon" />
         </a>
       </div>
       <div class="self-center">
-        <a type="button" class="text-left text-xl font-bold rounded-xl pl-4" href="https://packs.netlify.com">Packs</a>
+        <a type="button" class="text-left text-xl font-bold rounded-xl pl-4 transform transition duration-150 ease-in-out hover:scale-110" href="https://packs.netlify.com">Packs</a
+        >
       </div>
     </nav>
   </div>
   <div>
-    <h1 class="py-16 px-36 font-bold text-7xl animate-pulse">Hello World !</h1>
+    <h1 class="py-16 px-36 font-bold text-7xl transition duration-500 ease-in-out text-blue-600 hover:text-red-600 transform">Hello World !</h1>
     <h2 class="py-4 px-36 font-semibold text-4xl">Best Packs</h2>
   </div>
   <div class="py-36 pl-32 pr-32 place-items-center grid grid-cols-4 grid-flow-row-dense gap-y-20 gap-x-20">
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
-    <div class="flex shadow-xl"><img class="rounded-3xl" src="images/pack.png" alt="" /></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
+    <div class="flex"><a href=""><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out hover:scale-125" src="images/pack.png" alt="" /></a></div>
   </div>
 </main>
 
