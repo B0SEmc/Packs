f5921014a86aea61638d83ecdd9a51a1e91e674d
Thibaut <thibaut.musslin@gmail.com>
2021-10-03 12:09:57 +0200

    New look

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,45 @@
 
 <!-- svelte-ignore a11y-invalid-attribute -->
 <main>
-  <div class="flex-1 flex flex-col">
-    <nav class="flex bg-gray-500 h-16 bg-opacity-80">
+  <div>
+    <nav class="flex mx-auto bg-black h-auto bg-opacity-20 shadow-xl">
+      <div>
+        <a href="https://packs.netlify.com">
+          <img class="pl-16 py-1.5" src="src\assets\Icon.png" alt="Icon" />
+        </a>
+      </div>
       <div class="self-center">
-        <a type="button" class="ml-4 text-white font-bold rounded-xl px-4 py-3" href="https://www.discord.com">Packs</a>
+        <a type="button" class="text-left text-xl font-bold rounded-xl pl-4" href="https://packs.netlify.com">Packs</a>
       </div>
     </nav>
+  </div>
+  <div>
+    <h1 class="py-16 px-36 font-bold text-7xl animate-pulse">Hello World !</h1>
+    <h2 class="py-4 px-36 font-semibold text-4xl">Best Packs</h2>
+  </div>
+  <div class="py-36 pl-32 pr-32 place-items-center grid grid-cols-4 grid-flow-row-dense gap-y-20 gap-x-20">
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
+    <div class="flex shadow-xl"><img class="rounded-3xl" src="src\assets\Vaes+15k+pack.png" alt="" /></div>
   </div>
 </main>
 
 <style>
-main {
-  font-family: Inter, sans-serif;
-}
+  main {
+    font-family: Inter, sans-serif;
+  }
+  :global(body) {
+    background: rgb(31, 41, 55);
+    color: white;
+    scrollbar-width: none;
+  }
+  :global()::-webkit-scrollbar {
+    display: none;
+  }
 </style>
