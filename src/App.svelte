32be7d9d18737ebcc680835e94bf5eb1aa80b464
Thibaut <thibaut.musslin@gmail.com>
2021-10-04 10:55:02 +0200

    More responsive

--- src/App.svelte
+++ src/App.svelte
@@ -22,7 +22,7 @@
     <h1 class="py-16 px-36 font-bold text-7xl transition duration-500 ease-in-out text-blue-600 transform hover:animate-pulse">Hello World !</h1>
     <h2 class="py-4 px-36 font-semibold text-4xl">Best Packs</h2>
   </div>
-  <div class="py-36 pl-32 pr-32 place-items-center grid grid-cols-4 grid-flow-row-dense gap-y-20 gap-x-20">
+  <div class="py-36 pl-32 pr-32 place-items-center grid grid-cols-4 grid-flow-row-dense gap-y-20 gap-x-20 md:gap-x-10 md:gap-y-10 sm:gap-x-1 sm:gap-y-1">
     <div class="group flex">
       <a href=""
         ><img class="rounded-3xl shadow-2xl transform transition duration-300 ease-in-out group-hover:scale-125 filter group-hover:blur group-hover:opacity-50" src="images/pack{getRandomNum(5)}.png" alt="" />
