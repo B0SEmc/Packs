041727725bf3cdf22839fae9bdeed369f4830a16
Thibaut <thibaut.musslin@gmail.com>
2021-11-05 22:29:53 +0100

    Netlify Compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
